7824e402d83bc31117b23ce3855049da953d08aa
Rick Battipede <rbattipede@gmail.com>
2018-01-22 15:21:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+kittens =  ["Milo", "Otis", "Garfield"]
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendName(name){
+  return [...kittens, name]
+}
+
+
+function prependName(name){
+  return [name, ...kittens]
+}
+
+
+
